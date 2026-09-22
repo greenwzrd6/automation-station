90df35282cd8683ce4af63f06b67d497e2d036d2
felix.andersson <felix.andersson@DESKTOP-NTG4L42>
2026-09-22 14:24:05 +0200

    added structure and generated code

---
+++ Automation.Application/Events/ProcessPlacementCreatedEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automation.Application.Events
+{
+    internal class ProcessPlacementCreatedEventHandler
+    {
+    }
+}
---
+++ Automation.Application/Models/ActionContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automation.Application.Models
+{
+    internal class ActionContext
+    {
+    }
+}
